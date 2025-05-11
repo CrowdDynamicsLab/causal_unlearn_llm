1b63997704ed7a296254d4ae6bb0c5506c1c2d72
Yuen Chen <chenyuen0103@gpua056.delta.ncsa.illinois.edu>
2025-05-11 16:51:53 -0500

    to change branch

--- causal_to_concept/llms/get_activations/get_activations.sh
+++ causal_to_concept/llms/get_activations/get_activations.sh
@@ -1,5 +1,5 @@
-python get_activations.py --model_name llama3_8B --dataset_name toxigen_vicuna
-python get_activations.py --model_name llama3_8B --dataset_name hate_vicuna
+# python get_activations.py --model_name llama3_8B --dataset_name toxigen_vicuna
+# python get_activations.py --model_name llama3_8B --dataset_name hate_vicuna
 
 python get_activations.py --model_name llama_7B --dataset_name toxigen_vicuna
 python get_activations.py --model_name llama_7B --dataset_name hate_vicuna
